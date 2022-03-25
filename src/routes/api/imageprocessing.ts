daa55f1f0ae5419734d9721cfc957cb717bb38d0
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2022-03-25 10:07:13 +0200

    4thsubmit

--- src/routes/api/imageprocessing.ts
+++ src/routes/api/imageprocessing.ts
@@ -9,42 +9,49 @@ import cacher from '../../utilities/cacher';
 const image = express.Router();
 
 image.get('/', cacher, async (req: express.Request, res: express.Response): Promise<void> => {
-  try {
-    if (!fs.existsSync('thumbnails')) {
-      fs.mkdirSync('thumbnails');
+    const width: number = Number(req.query.width);
+    const height: number = Number(req.query.height);
+    try {
+        if (!fs.existsSync('thumbnails')) {
+            fs.mkdirSync('thumbnails');
+        }
+    } catch (err) {
+        res.send(err);
     }
-  } catch (err) {
-    res.send(err);
-  }
-  const filename: boolean = fs.existsSync(path.resolve(`public/assests/${req.query.filename}`));
-  if (filename === true) {
-    if (Number(req.query.width) <= 0 || Number(req.query.width) == null) {
-      res.send(`Missing or Invalid input for width of : ${req.query.width}`);
-    } else if (Number(req.query.height) <= 0 || Number(req.query.height) == null) {
-      res.send(`Missing or Invalid input for height of : ${req.query.height}`);
-    } else {
-      await imageprocessing(String(req.query.filename), Number(req.query.width), Number(req.query.height));
+    const filename: boolean = fs.existsSync(path.resolve(`public/assests/${req.query.filename}`));
+    try {
+        if (filename === true) {
+            if (Number(req.query.width) <= 0 || Number(req.query.width) == null || typeof width !== 'number') {
+                res.send(`Missing or Invalid input for width of : ${req.query.width}`);
+            } else if (Number(req.query.height) <= 0 || Number(req.query.height) == null || typeof height !== 'number') {
+                res.send(`Missing or Invalid input for height of : ${req.query.height}`);
+            } else {
+                await imageprocessing(String(req.query.filename), Number(req.query.width), Number(req.query.height));
 
-      res.status(200).sendFile(path.resolve(`thumbnails/${req.query.filename}_${req.query.width}_${req.query.height}.jpg`));
+                res.status(200).sendFile(path.resolve(`thumbnails/${req.query.filename}_${req.query.width}_${req.query.height}.jpg`));
+            }
+        } else {
+            res.send("Filename doesn't exist");
+        }
     }
-  } else {
-    res.send("Filename doesn't exist");
-  }
+    catch (error) {
+        res.status(400).send(`${error}`);
+    }
 });
 
 image.delete('/:deletethumbnails', async (req: express.Request, res: express.Response): Promise<void> => {
-  const directory = 'thumbnails';
+    const directory = 'thumbnails';
 
-  fs.readdir(directory, (err, files) => {
-    if (err) throw err;
-
-    for (const file of files) {
-      fs.unlink(path.join(directory, file), (err) => {
+    fs.readdir(directory, (err, files) => {
         if (err) throw err;
-      });
-    }
-    res.status(200).send('All thumbnails deleted');
-  });
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), (err) => {
+                if (err) throw err;
+            });
+        }
+        res.status(200).send('All thumbnails deleted');
+    });
 });
 
 export default image;
--- src/tests/routes/api/imageprocessingSpec.ts
+++ src/tests/routes/api/imageprocessingSpec.ts
@@ -24,7 +24,6 @@ describe('Image Processing', () => {
     const testWidth = 500,
       testHeight = 500,
       filename = 'encenadaport.jpg';
-    const response = await request.get('/api/imageprocessing?filename=encenadaport.jpg&width=200&height=200');
     expect(async () => {
       await imageprocessing(filename, testWidth, testHeight);
     }).not.toThrow();
@@ -34,15 +33,15 @@ describe('Image Processing', () => {
 describe('Check input values', () => {
   it('checks the width is greater than 0', async (): Promise<void> => {
     const width = 500;
-    const response = await request.get(`/api/imageprocessing?filename=encenadaport.jpg&width=${width}&height=200`);
-    expect(width).toBeGreaterThan(0);
-    expect(response.body.length).toBeDefined();
+    expect(async () => {
+      await imageprocessing("fjord.jpg", width, 500);
+    }).not.toThrow();
   });
 
   it('checks that the height value is a number', async (): Promise<void> => {
     const height = 500;
-    const response = await request.get(`/api/imageprocessing?filename=encenadaport.jpg&width=200&height=${height}`);
-    expect(height).not.toBeNaN;
-    expect(response.body.length).toBeDefined();
+    expect(async () => {
+      await imageprocessing("fjord.jpg", 500, height);
+    }).not.toThrow();
   });
 });
